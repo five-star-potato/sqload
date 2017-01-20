3adbbecd913ee0a268b0e084e141db7229037ee0
Cecil Lew <formpotato@yahoo.com>
2017-01-20 15:54:19 -0500

    project load fixed

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -6,7 +6,7 @@ import { DataGenerator, fnGetDataTypeDesc } from './include';
 import * as gen from './generator/generators.component';
 import { WizardStateService } from "./service/wizard-state";
 import { SampleDataService } from "./service/sample-data";
-import { ColumnDef, DBObjDef, ProjectService } from "./service/project";
+import { ColumnDef, DBObjDef, ProjectService, ProjectStruct } from "./service/project";
 import { Subscription }   from 'rxjs/Subscription';
 
 @Component({
@@ -58,7 +58,7 @@ export class HomeComponent extends BaseComponent implements OnInit {
             .then(result => {
                 // fix the loaded project file. Make sure its in zone.run, otherwise all databinding will file when you hit tables or columns page.
                 this.ngZone.run(() => {
-                    let project: any = this.projectService.project;
+                    let project: ProjectStruct = this.projectService.project;
                     let fileData = JSON.parse(result.data, this.reviver);
                     let data = fileData.project;
                     document.getElementById("projectTitle").innerHTML = result.filename;
@@ -68,23 +68,25 @@ export class HomeComponent extends BaseComponent implements OnInit {
                     project.connection.password = data.connection.password;
                     project.connection.verified = data.connection.verified;
 
-                    project.selectedTables = data.selectedTables;
-                    data.selectedTables.forEach(t => {
-                        let cols = data.columnDefs[t.id];
-                        for (let i = cols.length - 1; i >= 0; i--) {
-                            let c = cols[i];
-                            let realColDef: ColumnDef = Object.assign({}, c);
-                            cols[i] = realColDef;  
-                            if (c.plugIn.length > 0) {
-                                let obj = c.plugIn[0];
-                                let realPlug: any = new gen[obj.__name__](); // all the components within the module "gen" is accessible through [] indexer.
-                                // Object.assign can't seem to handle string to date conversino
-                                Object.assign(realPlug, obj);
-                                realColDef.plugIn.splice(0, 1, realPlug);
+                    project.selectedObjs = data.selectedObjs;
+                    for (let objType in project.selectedObjs) {
+                        data.selectedObjs[objType].forEach(obj => {
+                            let cols = data.columnDefs[obj.id];
+                            for (let i = cols.length - 1; i >= 0; i--) {
+                                let c = cols[i];
+                                let realColDef: ColumnDef = Object.assign({}, c);
+                                cols[i] = realColDef;  
+                                if (c.plugIn.length > 0) {
+                                    let obj = c.plugIn[0];
+                                    let realPlug: any = new gen[obj.__name__](); // all the components within the module "gen" is accessible through [] indexer.
+                                    // Object.assign can't seem to handle string to date conversino
+                                    Object.assign(realPlug, obj);
+                                    realColDef.plugIn.splice(0, 1, realPlug);
+                                }
                             }
-                        }
-                        project.columnDefs[t.id] = cols;
-                    });
+                            project.columnDefs[obj.id] = cols;
+                        });
+                    }
                     this.wizardStateService.projectChange({ type: TRON_EVENT.refresh });
                     document.getElementById("divProjectTitle").style.display = "";
                     this.router.navigate(['/connect']);
