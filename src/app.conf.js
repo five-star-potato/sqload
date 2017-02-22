f50acbd90054beb991266d13e34f361d86ce5745
Cecil Lew <five-star-potato@users.noreply.github.com>
2017-02-21 22:29:00 -0500

    .

---
+++ src/app.conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+    options : {
+        sqlOutputDir: "c:/wutemp/sql",
+        sqlFileMaxInserts: 10000
+    },
+    dataService : { 
+        url: "http://localhost:8081" ,
+        token: ""
+    },
+    database : {
+        serverName: 'DELL',
+        instanceName: '',
+        databaseName: 'HrhCleanerDb_Cecil',
+        userName: 'sa',
+        password: 'LongLive1'
+    }
+    
+};
