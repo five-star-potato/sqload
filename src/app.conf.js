745ad8eda72f42014b063545988b50c39116eef3
Cecil Lew <five-star-potato@users.noreply.github.com>
2017-02-21 22:30:22 -0500

    .

---
+++ src/app.conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+    options : {
+        sqlOutputDir: "c:/wutemp/sql",
+        sqlFileMaxInserts: 10000
+    },
+    dataService : { 
+        url: "http://localhost:8081" ,
+        token: ""
+    },
+    database : {
+        serverName: '',
+        instanceName: '',
+        databaseName: '',
+        userName: '',
+        password: ''
+    }
+    
+};
